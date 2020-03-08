831a0ea70d56ad9d29811874afc6b74d082a559a
arondasamuel123 <arondasamuel123@gmail.com>
2020-03-08 18:03:40 +0300

    A user can post an image

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # path('activate/<str:uidb64>/<str:token>/',views.activate, name='activate'),
     path('profile/', views.profile, name='create-profile'),
     path('profile/<int:id>', views.profile_user, name='dipslay-profile'),
+    path('post/image/',views.post_image,name='post-image'),
     path('logout/', views.logout_view, name='logout')
 
 ]
